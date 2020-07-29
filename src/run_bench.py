879295ea7b69ac18a53d87cd37003ad31a34fd67
Nicola Esposito <33328103+esposni@users.noreply.github.com>
2020-07-29 12:22:57 +0200

    mod run bench

--- src/run_bench.py
+++ src/run_bench.py
@@ -32,26 +32,27 @@ def runBench():
     os.popen("mkdir batch").read()
     count=0
     for chunk in bin_chunks:
-        job_name="SCOPF_"+str(count)
-        batch_file="batch/job_"+job_name+".sh"
-        f =open(batch_file,"w+")
-        f.write("#!/bin/sh\n\n")
-        f.write("#SBATCH --job-name="+job_name+"\n")
-        f.write("#SBATCH -p %s\n" %partition)
-        f.write("#SBATCH -n 1\n")
-        f.write("#SBATCH --time=10:20:59\n")
-        f.write("#SBATCH --mem=64148\n")
-        f.write("#SBATCH --exclusive\n")
-        f.write("#SBATCH -o ./out/slurm_"+job_name+"-%j.out\n")
-        f.write("#SBATCH -e ./err/slurm_"+job_name+"-%j.err\n")
-        # ----------------------------------------------
-        for rd in chunk:
-            case_name=rd.replace('.m','')
-            f.write("julia run_powermodel.jl "+mypath+rd+" >> logs/log_"+case_name+".out\n")
-        f.close()
-       	job = os.popen("sbatch " + batch_file).read()
-        print(job)
-        count+=1
+        if(chunk!=[]):
+            job_name="SCOPF_"+str(count)
+            batch_file="batch/job_"+job_name+".sh"
+            f =open(batch_file,"w+")
+            f.write("#!/bin/sh\n\n")
+            f.write("#SBATCH --job-name="+job_name+"\n")
+            f.write("#SBATCH -p %s\n" %partition)
+            f.write("#SBATCH -n 1\n")
+            f.write("#SBATCH --time=10:20:59\n")
+            f.write("#SBATCH --mem=64148\n")
+            f.write("#SBATCH --exclusive\n")
+            f.write("#SBATCH -o ./out/slurm_"+job_name+"-%j.out\n")
+            f.write("#SBATCH -e ./err/slurm_"+job_name+"-%j.err\n")
+            # ----------------------------------------------
+            for rd in chunk:
+                case_name=rd.replace('.m','')
+                f.write("julia run_powermodel.jl "+mypath+rd+" >> logs/log_"+case_name+".out\n")
+            f.close()
+            job = os.popen("sbatch " + batch_file).read()
+            print(job)
+            count+=1
  
 if (__name__ == '__main__'):
     runBench()
