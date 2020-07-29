8401b06b396cfd318dd1c005e3bc7e5545e2f06a
Nicola Esposito <33328103+esposni@users.noreply.github.com>
2020-07-29 12:25:48 +0200

    mod

--- src/processIPOPT.py
+++ src/processIPOPT.py
@@ -8,6 +8,8 @@ f = open(log_file, "r")
 print("Results:")
 for line in f:
     if "Number of Iterations....:" in line and option=="-i":
-        print(line)
+        out=line.replace("Number of Iterations....: ","")
+        print(out)
     elif "Time to Solution........:" in line and option=="-t":
-        print(line)
+        out=line.replace("Number of Iterations....: ","")
+        print(out)
