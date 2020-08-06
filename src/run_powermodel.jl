65f5cd18a7f246575814c423512385b0c890f31f
Nicola Esposito <33328103+esposni@users.noreply.github.com>
2020-08-06 16:37:14 +0200

    formulation julia

--- src/run_powermodel.jl
+++ src/run_powermodel.jl
@@ -4,12 +4,33 @@ using KNITRO
 
 in_case=ARGS[1]
 optimizer=ARGS[2]
+formulation=ARGS[2]
 println(in_case)
 println(optimizer)
+println(formulation)
+
+if (formulation=="ACP")
+	type_formulation = ACPPowerModel
+elseif (formulation=="DCP")
+	type_formulation = DCPPowerModel
+elseif (formulation=="IVR")
+	type_formulation = IVRPowerModel
+elseif (formulation=="SOCW")
+	type_formulation = SOCWRPowerModel
+elseif (formulation=="QCRM")
+	type_formulation = QCRMPowerModel
+elseif (formulation=="SOCBF")
+	type_formulation = SOCBFPowerModel
+else
+	println("Error: wrong formulation")
+end
+
 if (optimizer=="knitro")
-	result = run_ac_opf(in_case, with_optimizer(KNITRO.Optimizer))
+	result = run_opf("matpower/case3_dc.m", type_formulation, with_optimizer(Ipopt.Optimizer))
 elseif (optimizer=="ipopt")
-	result = run_ac_opf(in_case, with_optimizer(Ipopt.Optimizer))
+	result = run_opf("matpower/case3_dc.m", type_formulation, with_optimizer(Ipopt.Optimizer))
+else
+	println("Error: wrong optimizer")
 end
 
 println("Time to Solution........:",result["solve_time"])
